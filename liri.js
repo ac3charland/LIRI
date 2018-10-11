c3c01f55218db115155eb4809d3544b4b786524f
Alex Charland <ac3charland@gmail.com>
2018-10-11 11:34:02 -0500

    Created placeholder functions for each AJAX request.

--- liri.js
+++ liri.js
@@ -2,18 +2,37 @@ require("dotenv").config();
 import * as keys from 'keys.js';
 
 var command = process.argv[2];
+var argument = process.argv[3];
+
+function bandsInTown(artist) {
+
+}
+
+function spotify(songName) {
+
+}
+
+function omdb(movieName) {
+
+}
+
+function fs(argument) {
+
+}
 
 switch (command) {
     case "concert-this":
-        //
+        bandsInTown(argument);
         break;
     case "spotify-this-song":
-        //
+        spotify(argument);
         break;
     case "movie-this":
-        //
+        omdb(argument);
         break;
     case "do-what-it-says":
-        //
+        fs(argument);
         break;
+    default:
+        console.log("Unrecognized command.")
 }
